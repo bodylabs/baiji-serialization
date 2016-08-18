bfbf3d6fa5341301c5bbdbf951bc4f4da7705375
Martin Kristiansen <martin.kristiansen@bodylabs.com>
2016-08-18 13:34:10 -0400

    Lint and cleaning up

--- baiji/serialization/test_yaml.py
+++ baiji/serialization/test_yaml.py
@@ -1,7 +1,5 @@
-from __future__ import absolute_import
-import yaml as pyyaml
 import unittest
-from baiji.serialization import yaml as baiji_yaml
+from baiji.serialization import yaml
 
 class UnsafeToDump(object):
     """Python classes should not by default be YAML dumpable in safe mode"""
@@ -12,17 +10,17 @@ unsafe_to_load = "!!python/name:baiji.serialization.test_yaml.UnsafeToDump ''\n"
 class TestYAML(unittest.TestCase):
 
     def test_yaml_unsafe_dumps(self):
-        self.assertEqual(baiji_yaml.dumps(UnsafeToDump), unsafe_to_load)
+        self.assertEqual(yaml.dumps(UnsafeToDump), unsafe_to_load)
 
     def test_yaml_unsafe_loads_uses_load(self):
-        self.assertEqual(baiji_yaml.loads(unsafe_to_load), UnsafeToDump)
+        self.assertEqual(yaml.loads(unsafe_to_load), UnsafeToDump)
 
     def test_yaml_safe_dump_raises_on_unsafe(self):
         self.assertRaises(
-            baiji_yaml.SerializationSafetyError,
-            baiji_yaml.dumps, UnsafeToDump, safe_dump=True)
+            yaml.SerializationSafetyError,
+            yaml.dumps, UnsafeToDump, safe_dump=True)
 
     def test_yaml_safe_load_raises_on_unsafe(self):
         self.assertRaises(
-            baiji_yaml.SerializationSafetyError,
-            baiji_yaml.loads, unsafe_to_load, safe_load=True)
+            yaml.SerializationSafetyError,
+            yaml.loads, unsafe_to_load, safe_load=True)
