d5d8136ea68f201480916e4de01a54c096241233
Martin Kristiansen <martin.kristiansen@bodylabs.com>
2016-08-18 12:13:22 -0400

    YAML safe load and dump

--- baiji/serialization/yaml.py
+++ baiji/serialization/yaml.py
@@ -15,19 +15,31 @@ def load(f, *args, **kwargs):
 
 def loads(s, *args, **kwargs):
     import yaml
+    safe_load = kwargs.pop('safe_load', False) # Perhaps default should be True?
+    if safe_load:
+        return yaml.safe_load(s, *args, **kwargs)
     return yaml.load(s, *args, **kwargs)
 
 
 def dumps(obj, *args, **kwargs):
     import yaml
+    safe_dump = kwargs.pop('safe_dump', False) # Perhaps default should be True?
+    if safe_dump:
+        return yaml.safe_dump(obj, *args, **kwargs)
     return yaml.dump(obj, *args, **kwargs)
 
 
 def _dump(f, obj, *args, **kwargs):
     import yaml
+    safe_dump = kwargs.pop('safe_dump', False) # Perhaps default should be True?
+    if safe_dump:
+        return yaml.safe_dump(obj, f, *args, **kwargs)
     return yaml.dump(obj, f, *args, **kwargs)
 
 
 def _load(f, *args, **kwargs):
     import yaml
+    safe_load = kwargs.pop('safe_load', False) # Perhaps default should be True?
+    if safe_load:
+        return yaml.safe_load(f, *args, **kwargs)
     return yaml.load(f, *args, **kwargs)
